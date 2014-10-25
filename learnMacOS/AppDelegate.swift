18997f8e850d86668d4fcfd1db9b0251982a3be0
James Waldrop <jw@twitter.com>
2014-10-25 15:03:15 -0700

    initial commit

---
+++ learnMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  learnMacOS
+//
+//  Created by James Waldrop on 10/25/14.
+//  Copyright (c) 2014 Hive Theory. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ learnMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  learnMacOS
+//
+//  Created by James Waldrop on 10/25/14.
+//  Copyright (c) 2014 Hive Theory. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
